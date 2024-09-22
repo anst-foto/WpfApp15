ee7b626fa0454f3fd01fb504856fce636ffae0e7
Starinin Andrey <starininandrey@gmail.com>
2024-09-22 13:27:07 +0300

    Initial commit

---
+++ WpfApp1/Account.cs
@@ -0,0 +1,23 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace WpfApp1;
+
+public record Account
+{
+    [JsonPropertyName("id")]
+    public Guid Id { get; set; }
+
+    [JsonPropertyName("login")]
+    public string Login { get; set; }
+
+    [JsonPropertyName("password")]
+    public string Password { get; set; }
+
+    [JsonPropertyName("role")]
+    public string Role { get; set; }
+
+    public static Account FromJson(string json) =>
+        JsonSerializer.Deserialize<Account>(json) ?? throw new Exception("Failed to deserialize account");
+    public static string ToJson(Account account) => JsonSerializer.Serialize(account);
+}
